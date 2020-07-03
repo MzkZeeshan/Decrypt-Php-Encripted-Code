c9f83db728f4602ab8c54fad78fa9d0b8aa48521
MzkZeeshan <zeeshankhanaptech@gmail.com>
2020-07-03 12:17:20 +0500

    added

--- config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // database: "mongodb://localhost:27017/Click"
-  database: "mongodb+srv://mzk:mzk123@cluster0-4mhba.mongodb.net/clickcshopping?retryWrites=true&w=majority"
-};
--- routes/user.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/get", (res, req) => console.log("user work"));
-
-module.exports = router;
